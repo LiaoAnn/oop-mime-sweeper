3f50d39d167e24f7963260185e9dbf36158520f7
yaoenliu <peterliu1060@gmail.com>
2023-04-17 08:40:20 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "QtWidgetsApplication.h"
 #include <QtWidgets/QApplication>
-
-int main(int argc, char *argv[])
+#include <ctime>
+#include <string>
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    QtWidgetsApplication w;
-    w.show();
-    return a.exec();
+	QApplication a(argc, argv);
+	QtWidgetsApplication w;
+	w.show();
+	// Set size and lock it
+	w.resize(320, 330);
+	w.setMaximumSize(740, 750);
+	w.setMinimumSize(740, 750);
+	w.move(100, 100);
+	time_t t = time(0);
+	// set window title as now time
+	QString Title = QString::fromStdString("build time : ") + ctime(&t);
+	w.setWindowTitle(Title);
+	return a.exec();
 }
